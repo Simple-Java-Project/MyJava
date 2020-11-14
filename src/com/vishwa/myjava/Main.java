66adbc376426d0fcc904ab4e47fa327a41e623a2
Viswanattha Reddy N <viswanathareddynagireddy@gmail.com>
2020-11-14 21:19:06 +0530

    artifacts are created

--- src/com/vishwa/myjava/Main.java
+++ src/com/vishwa/myjava/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("am Vishwa");
-        System.out.println("am the second line in Java");
+        System.out.println("edited artifacts and simple project completed");
     }
 }
