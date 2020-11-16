f2929655404b37cca78f2ac702d81f4dd700c23b
Viswanatha Reddy Nagireddy <viswanathareddynagireddy@gmail.com>
2020-11-16 12:26:35 +0530

    added Github Desktop
    Github Desktop App has been added to my Laptop

--- src/com/vishwa/myjava/Main.java
+++ src/com/vishwa/myjava/Main.java
@@ -6,5 +6,6 @@ public class Main {
 	// write your code here
         System.out.println("am Vishwa");
         System.out.println("edited artifacts and simple project completed");
+	System.out.println(""added Github Desktop);
     }
 }
